ca42ad128caf1a747bc6acc8fa70f126a535d2d6
Will Chiong <will@infomofo.com>
2013-03-30 23:26:23 -0400

    Add some responsive css for hiding extra stats on mobile devices

--- v1/js/controllers.js
+++ v1/js/controllers.js
@@ -56,4 +56,72 @@ function PartyController($scope, $http, $location) {
         attack += member.character.bonus.attack;
     return attack;
   }
+
+  $scope.computeXpPct = function(member) {
+    var xp_pct = 100;
+    if (member.character.hasOwnProperty("bonus"))
+      if(member.character.bonus.hasOwnProperty("xp_pct"))
+        xp_pct += member.character.bonus.xp_pct;
+    return xp_pct;
+  }
+
+  $scope.computeHpPct = function(member) {
+    var hp_pct = 0;
+    if (member.character.hasOwnProperty("bonus"))
+      if(member.character.bonus.hasOwnProperty("hp_pct"))
+        hp_pct += member.character.bonus.hp_pct;
+    return hp_pct;
+  }
+
+  $scope.computeMpPct = function(member) {
+    var mp_pct = 0;
+    if (member.character.hasOwnProperty("bonus"))
+      if(member.character.bonus.hasOwnProperty("mp_pct"))
+        mp_pct += member.character.bonus.mp_pct;
+    return mp_pct;
+  }
+
+  $scope.computeHp = function(member) {
+    var hp = member.job.hp;
+    if (member.character.hasOwnProperty("bonus"))
+      if(member.character.bonus.hasOwnProperty("hp"))
+        hp += member.character.bonus.hp;
+    var bonus = .01 * $scope.computeHpPct(member) * hp;
+    hp += bonus;
+    return hp;
+  }
+
+  $scope.computeThreat = function(member) {
+    var threat = member.job.threat;
+    if (member.character.hasOwnProperty("bonus"))
+      if(member.character.bonus.hasOwnProperty("threat"))
+        threat += member.character.bonus.threat;
+    return threat;
+  }
+
+  $scope.computeMp = function(member) {
+    var mp = member.job.mp;
+    if (member.character.hasOwnProperty("bonus"))
+      if(member.character.bonus.hasOwnProperty("mp"))
+        mp += member.character.bonus.mp;
+    var bonus = .01 * $scope.computeMpPct(member) * mp;
+    mp += bonus;
+    return mp;
+  }
+
+  $scope.computeInitiative = function(member) {
+    var initiative = member.job.initiative;
+    if (member.character.hasOwnProperty("bonus"))
+      if(member.character.bonus.hasOwnProperty("initiative"))
+        initiative += member.character.bonus.initiative;
+    return initiative;
+  }
+
+  $scope.computeCriticalPct = function(member) {
+    var critical_pct = member.job.critical_pct;
+    if (member.character.hasOwnProperty("bonus"))
+      if(member.character.bonus.hasOwnProperty("critical_pct"))
+        critical_pct += member.character.bonus.critical_pct;
+    return critical_pct;
+  }
 }
